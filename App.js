72364d6abcd5af15c62cb674f14abaa1281d8d5f
Venkatesh <venkateshnalla94@gmail.com>
2020-10-09 04:11:47 +0530

    Introducing ContextApi into the Project.

--- App.js
+++ App.js
@@ -1,21 +1,21 @@
-import { StatusBar } from 'expo-status-bar';
+import { createAppContainer } from 'react-navigation';
+import { createStackNavigator } from 'react-navigation-stack';
+import IndexScreen from "./src/screens/IndexScreen";
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {BlogProvider} from "./src/context/BlogContext";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+const navigator=createStackNavigator({
+  Index:IndexScreen
+},{
+  defaultNavigationOptions:{
+    title:"Index"
+  }
+})
+
+const App= createAppContainer(navigator);
+
+export default ()=>{
+  return <BlogProvider>
+    <App/>
+  </BlogProvider>
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
