c68fb8594222d30fb6833aac1d7116eeab8e5d77
Venkatesh <venkateshnalla94@gmail.com>
2020-10-10 06:56:55 +0530

    Reference to github Backend Project

--- src/api/jsonServer.js
+++ src/api/jsonServer.js
@@ -2,4 +2,9 @@ import axios from 'axios';
 
 export default axios.create({
      baseURL:'http://22e76ad682ff.ngrok.io'
+     /**
+      * Always check with
+      * https://github.com/venkateshnalla94/BlogServer
+      * to know which Tunnel URL to use
+      */
 })
